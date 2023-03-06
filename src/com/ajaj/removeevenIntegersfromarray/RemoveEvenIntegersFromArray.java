a1d2e034c749cbdaa7c189671cc0da603966f9d4
Admin <admin@Ajas-MacBook-Pro.local>
2023-03-06 18:15:10 +0530

    dsalgos

---
+++ src/com/ajaj/removeevenIntegersfromarray/RemoveEvenIntegersFromArray.java
@@ -0,0 +1,67 @@
+package com.ajaj.removeevenIntegersfromarray;
+
+import java.util.Iterator;
+
+public class RemoveEvenIntegersFromArray {
+	
+	public static void main(String[] args) {
+		
+		int[] array = {2,3,4,5,6,7};
+		//removeEvenIntegersFromArray(array);
+		removeEvenIntegersFromArrayy(array);
+	}
+	
+	public static void removeEvenIntegersFromArray(int[] array) {
+		
+		int oddSize = 0;
+		
+		for( int i=0; i<array.length; i++) {
+			if(array[i]%2 !=0) {
+				oddSize++;
+			}
+		}
+		
+		int[] result = new int[oddSize];
+		int index = 0;
+		for (int i = 0; i < array.length; i++) {
+			
+			if(array[i] % 2 != 0) {
+				result[index]=array[i];
+				System.out.println(array[i]);
+				index++;
+			}
+			
+		}
+		
+	//	System.out.println(oddSize);
+		System.out.println(result);
+	}
+	
+	
+	public static void removeEvenIntegersFromArrayy(int[] array) {
+	
+		int oddsize =0;
+		for (int i=0; i < array.length; i++) {
+			if(array[i]%2 != 0) {
+				oddsize++;
+			}
+		}
+		
+		int[] result = new int[oddsize];
+		int index = 0;
+		
+		for (int i=0; i<array.length ; i++) {
+			
+			if(array[i]%2 !=0) {
+				result[index] = array[i];
+				index++;
+			}
+			
+		}
+		
+		for (int i : result) {
+			System.out.println(i);
+		}
+	}
+
+}
